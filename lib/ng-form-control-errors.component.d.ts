3b9ba85bf8e1c38731e2ed99dbd9eca29f1d8b26
Dmitrij Kuba <dimakuba124@gmail.com>
2017-11-12 17:24:40 +0200

    compile to es5

---
+++ lib/ng-form-control-errors.component.d.ts
@@ -0,0 +1,19 @@
+import { ChangeDetectorRef, OnInit, TemplateRef } from '@angular/core';
+import { FormControl } from "@angular/forms";
+export interface Error {
+    name: string;
+    message: string;
+}
+export declare type Errors = {
+    [error: string]: string;
+};
+export declare class NgFormControlErrorsComponent implements OnInit {
+    private cd;
+    control: FormControl;
+    errors: Errors;
+    template: TemplateRef<any>;
+    error: Error;
+    constructor(cd: ChangeDetectorRef);
+    ngOnInit(): void;
+    detectErrors(): void;
+}
