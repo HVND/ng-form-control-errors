1326d0a769b7cfe4afa97c42fab924ea79c83806
Dmitrij Kuba <dimakuba124@gmail.com>
2017-11-12 22:00:18 +0200

    add index.js

---
+++ index.d.ts
@@ -0,0 +1,2 @@
+export * from './lib/ng-form-control-errors.module';
+export * from './lib/ng-form-control-errors.component';
---
+++ index.js
@@ -0,0 +1,7 @@
+"use strict";
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+__export(require("./lib/ng-form-control-errors.module"));
+__export(require("./lib/ng-form-control-errors.component"));
+//# sourceMappingURL=index.js.map
