db68ab740d162613c3d8ddfa8f4fe1cdd89e607c
Dmitrij Kuba <dimakuba124@gmail.com>
2017-12-13 10:18:00 +0200

    support for FormControlDirective, NgModel

--- src/app/ng-form-control-errors/ng-form-control-errors-content.ts
+++ src/app/ng-form-control-errors/ng-form-control-errors-content.ts
@@ -1,5 +1,5 @@
 import {ContentChild, Directive} from '@angular/core';
-import {FormControl, FormControlName} from "@angular/forms";
+import {FormControl, FormControlDirective, FormControlName, NgModel} from "@angular/forms";
 
 @Directive({
     selector: '.control-errors-content',
@@ -7,9 +7,27 @@ import {FormControl, FormControlName} from "@angular/forms";
 })
 export class NgFormControlErrorsContent {
 
-    @ContentChild(FormControlName) formControlName: FormControlName;
+    @ContentChild(FormControlName) _formCtrlName: FormControlName;
+
+    @ContentChild(FormControlDirective) _formCtrlDir: FormControlDirective;
+
+    @ContentChild(NgModel) _ngModel: NgModel;
 
     get formControl(): FormControl | null {
-        return (this.formControlName && ('control' in this.formControlName)) ? this.formControlName.control : null;
+        let _formControl = null;
+
+        if (this._formCtrlName) {
+            _formControl = this._formCtrlName;
+        } else if (this._formCtrlDir) {
+            _formControl = this._formCtrlDir;
+        } else if (this._ngModel) {
+            _formControl = this._ngModel;
+        }
+
+        if (_formControl) {
+            _formControl = ('control' in _formControl) ? _formControl.control : null;
+        }
+
+        return _formControl;
     }
 }
